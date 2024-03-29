358e6a0daf666cf9b99b0e9118a98dd7f44f2c6e
DESKTOP-F4BC13L\hi <snake.ohgiraffers@gmail.com>
2024-03-29 13:16:36 +0900

    static

--- 01_JAVA/chap06-class-and-object-lecture-source/src/com/ohgiraffers/section05/parameter/Application.java
+++ 01_JAVA/chap06-class-and-object-lecture-source/src/com/ohgiraffers/section05/parameter/Application.java
@@ -33,5 +33,24 @@ public class Application {
         }
         System.out.println();
 
+        /* 3. 클래스 자료형 */
+        RectAngle r1 = new RectAngle(12.5,22.5);
+        System.out.println("인자로 전달하는 값 :" +r1);
+        pt.testClassType(r1);
+
+        System.out.println("변경 후 사각형의 넓이와 둘레=================");
+        r1.calcArea();
+        r1.calcRound();
+
+        /* 4. 클래스 자료형 배열은 뒤에 다룬다.*/
+
+        /* 5. 가변인자 */
+        //pt.testVariableLengthArray();
+        pt.testVariableLengthArray("홍길동"); //가변인자는 전달하지 않아도 됨
+        pt.testVariableLengthArray("유관순","볼링"); //1개도 전달가능
+        pt.testVariableLengthArray("이순신", "볼링","농구","축구"); //여러개도 가능
+        pt.testVariableLengthArray("신사임당",new String[]{"테니스","서예"}); // 베열도 가능
+
+
     }
 }
--- 01_JAVA/chap06-class-and-object-lecture-source/src/com/ohgiraffers/section05/parameter/ParameterTest.java
+++ 01_JAVA/chap06-class-and-object-lecture-source/src/com/ohgiraffers/section05/parameter/ParameterTest.java
@@ -27,6 +27,47 @@ public class ParameterTest {
 
     }
 
+    public void testClassType(RectAngle rectAngle){
+
+        System.out.println("매개변수로 전달받은 값 : " + rectAngle);
+
+        System.out.println("사각형의 넓이와 둘레====================");
+        rectAngle.calcArea();
+        rectAngle.calcRound();
+
+        rectAngle.setWidth(100);
+        rectAngle.setHeight(100);
+
+        System.out.println("변경된 사각형의 넓이와 둘레====================");
+        rectAngle.calcArea();
+        rectAngle.calcRound();
+
+    }
+
+    public void testVariableLengthArray(String name, String...hobby){
+
+        System.out.println("이름 : " +name);
+        System.out.println("취미의 갯수 : " + hobby.length);
+        System.out.print("취미 : ");
+        for(int i = 0; i<hobby.length; i++){
+            System.out.print(hobby[i] + " ");
+        }
+        System.out.println();
+
+
+    }
+
+/*    public void testVariableLengthArray(String...hobby){
+
+        System.out.println("취미의 갯수 : " + hobby.length);
+        System.out.print("취미 : ");
+        for(int i = 0; i<hobby.length; i++){
+            System.out.print(hobby[i] + " ");
+        }
+        System.out.println();
+
+    }*/
+
 
     
     
