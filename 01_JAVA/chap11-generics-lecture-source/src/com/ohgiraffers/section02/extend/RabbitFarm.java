8ccc01b38a706a7bd362ab982e5e90a3665ba107
DESKTOP-F4BC13L\hi <snake.ohgiraffers@gmail.com>
2024-04-04 10:14:59 +0900

    제네릭

---
+++ 01_JAVA/chap11-generics-lecture-source/src/com/ohgiraffers/section02/extend/RabbitFarm.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section02.extend;
+
+//public class RabbitFarm<T implements Animal> { //에러남
+//public class RabbitFarm<T extends Animal> { //정상
+public class RabbitFarm<T extends Rabbit> {
+
+
+
+}
---
+++ 01_JAVA/chap11-generics-lecture-source/src/com/ohgiraffers/section02/extend/run/Application1.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section02.extend.run;
+
+public class Application1 {
+    public static void main(String[] args) {
+        /* extends 키워드를 이용하여 특정한 타입만을 사용하도록 제네릭 범위를 제한할 수 있다.*/
+
+    }
+}
